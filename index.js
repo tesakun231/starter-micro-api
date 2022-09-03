28a3dc100a9886b561f16db59e9037339c713c25
tesakun231 <112684077+tesakun231@users.noreply.github.com>
2022-09-03 15:52:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-var os = require('os');
+const exec = require('child_process').exec, child;
+const myShellScript = exec('lscpu');
+myShellScript.stdout.on('data', (data)=>{
+    console.log(data); 
+    // do whatever you want here with data
+});
+myShellScript.stderr.on('data', (data)=>{
+    console.error(data);
+});
+
 var http = require('http');
 http.createServer(function (req, res) {
-    console.log(os.cpus());
-    console.log(os.totalmem());
-    console.log(os.freemem())
     console.log(`Just got a request at ${req.url}!`)
     res.write('Yo!'+os.cpus());
     res.end();
