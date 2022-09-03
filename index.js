4563bb947b2ab57080c02562e59f71a95b07e924
tesakun231 <112684077+tesakun231@users.noreply.github.com>
2022-09-03 16:02:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var http = require('http');
+const { spawn } = require('node:child_process');
+const ls = spawn('ls', ['-lh', '/usr']);
+
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     res.write('Yo man!');
     res.end();
 }).listen(process.env.PORT || 3000);
+
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
