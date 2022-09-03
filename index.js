176e7bc0f520a93770934520a86bb819138e60aa
tesakun231 <112684077+tesakun231@users.noreply.github.com>
2022-09-03 16:07:30 +0800

    a

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Hello World!');
-    console.log('ada');
+    console.log('adaa');
     res.end();
 }).listen(3000);
