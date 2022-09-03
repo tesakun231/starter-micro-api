0d5dc8fb48311e01e0b03a7aa3814d20776048aa
tesakun231 <112684077+tesakun231@users.noreply.github.com>
2022-09-03 16:11:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var http = require('http');
+const { spawn } = require('node:child_process');
+const ls = spawn('ls', ['-lh', '/usr']);
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
@@ -16,4 +18,15 @@ http.createServer(function (req, res) {
   </body>
 </html>`);
     console.log('print in Node.js engine');   
+    ls.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    ls.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    ls.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
 }).listen(3000);
