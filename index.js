18dfff25ce7b4b4e4bafbb42a61a6478bb27ab08
tesakun231 <112684077+tesakun231@users.noreply.github.com>
2022-09-03 15:44:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+var os = require('os');
 var http = require('http');
 http.createServer(function (req, res) {
+    console.log(os.cpus());
+    console.log(os.totalmem());
+    console.log(os.freemem())
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+    res.write('Yo!'+os.cpus());
     res.end();
 }).listen(process.env.PORT || 3000);
