cf0aa7571fabd1ab537463c46ba0eccbba325e94
tesakun231 <112684077+tesakun231@users.noreply.github.com>
2022-09-03 16:09:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World!');
-    console.log('adaa');
-    res.end();
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('' +
+`<html>
+  <head></head>
+  <body>
+    Data render at browser page
+    <script>
+      /********** Browser start ********/
+      /* This code is run in the browser */
+      console.log('print in browser console ');
+      /********** Browser end ********/
+    </script>
+  </body>
+</html>`);
+    console.log('print in Node.js engine');   
 }).listen(3000);
