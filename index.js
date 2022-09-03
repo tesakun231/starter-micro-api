99fe4d5c1b71616db89dcec6771235464c4e33e1
tesakun231 <112684077+tesakun231@users.noreply.github.com>
2022-09-03 15:55:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-const exec = require('child_process').exec, child;
-const myShellScript = exec('lscpu');
-myShellScript.stdout.on('data', (data)=>{
-    console.log(data); 
-    // do whatever you want here with data
-});
-myShellScript.stderr.on('data', (data)=>{
-    console.error(data);
-});
-
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!'+os.cpus());
+    res.write('Yo man!');
     res.end();
 }).listen(process.env.PORT || 3000);
